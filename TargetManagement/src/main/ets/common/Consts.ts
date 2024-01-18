afee9c67a59624dc3f13cc04ee78e2bc9acd960f
丁元帅 <yuanshuai@bertadata.com>
2024-01-18 11:32:33 +0800

    目标管理：进度管理

--- TargetManagement/src/main/ets/common/Consts.ts
+++ TargetManagement/src/main/ets/common/Consts.ts
@@ -3,3 +3,11 @@ export const WIDTH_FULL = '100%'
 export const HEIGHT_FULL = '100%'
 
 export const LIST_SPACE = 10
+
+export const TRANSITION_ANIMATION_X: number = 1;
+
+export const TRANSITION_ANIMATION_Y: number = 0;
+
+export const SLIDER_MODE_DEFAULT: number = -1;
+export const SLIDER_MODE_CLICK: number = 3;
+
