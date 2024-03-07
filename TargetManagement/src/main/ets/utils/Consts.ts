449223fee8cf61104a89d04c7d682bdfcb717392
丁元帅 <yuanshuai@bertadata.com>
2024-03-07 19:28:43 +0800

    视频播放：首页

--- TargetManagement/src/main/ets/utils/Consts.ts
+++ TargetManagement/src/main/ets/utils/Consts.ts
@@ -1,9 +1,3 @@
-export const WIDTH_FULL = '100%'
-
-export const HEIGHT_FULL = '100%'
-
-export const LIST_SPACE = 10
-
 export const TRANSITION_ANIMATION_X: number = 1;
 
 export const TRANSITION_ANIMATION_Y: number = 0;
